45b2e9a4c22bf115d051300285e2f04b574f39c8
Curtis Edwards <curtis-edwards>
2024-03-04 09:31:12 -0500

    Added code

--- index.ts
+++ index.ts
@@ -1,1 +1,19 @@
-console.log("Hello via Bun!");
+// get arguments
+//
+import { readFileSync } from 'fs';
+//
+// print process.argv
+process.argv.forEach(function (val, index, array) {
+  console.log(index + ': ' + val)
+})
+
+console.log(process.argv[2])
+
+
+const file = readFileSync(process.argv[2], 'utf8')
+console.log(file)
+
+const newArray = file.split(/\r?\n/)
+// pop last element, since it will be empty (the EOF)
+newArray.pop()
+console.log(newArray)
